508d017d047e2aa534446bfcd2609078a32ef7c9
GFsilva13 <genivaldo13@gmail.com>
2025-05-10 16:54:42 -0300

    Content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-small-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
@@ -11,6 +12,8 @@ export class SmallCardComponent implements OnInit{
   photoSmall: String = ""
   @Input()
   titleSmall: String = ""
+  @Input()
+  Id:String ="0"
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/data-fake.ts
@@ -0,0 +1,38 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "IMPERDÍVEL: NOVA VERSÃO DO ANGULAR 17",
+    "description":"Conheça as novas funcionalidades do Angular e esteja atualizado com o mercado.",
+    "photoCover":"https://miro.medium.com/v2/resize:fit:1200/1*bMgQ8MhbnQexpqHgIgBJPA.png"
+  },
+
+ {
+    "id":"2",
+    "title": "Imperdível: Conheça as novidades do HTML 5",
+    "description":"Conheça as novas funcionalidades do HTML 5.",
+    "photoCover":"https://wallpaperaccess.com/full/4868336.jpg"
+  },
+
+  {
+    "id":"3",
+    "title": "A Complete Guide to Angular!",
+    "description":"Guia completo para trabalhar com Angular.",
+    "photoCover":"https://tse2.mm.bing.net/th/id/OIP._-WQrBlEGUsBQjzMeR8v3QHaEJ?rs=1&pid=ImgDetMain"
+  },
+
+  {
+    "id":"4",
+    "title": "¿Por qué Angular es el mejor marco para el desarrollo de ERP?",
+    "description":"Conheça todaa as vantagens do Angular.",
+    "photoCover":"https://www.precognis.com/wp-content/uploads/2023/04/angular-4.webp"
+  },
+
+  {
+    "id":"5",
+    "title": "Criando aplicações Angular com Angular CLI",
+    "description":"Crie aplicaçoes Angular com Angula CLI.",
+    "photoCover":"https://bcbrito.com.br/wp-content/uploads/2023/01/Criando-aplicacoes-Angular-com-Angular-CLI-1024x576.jpg"
+  }
+]
+
+
